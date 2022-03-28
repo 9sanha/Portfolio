3dcecc4ef1434872d9290eadc4347b7402f6519b
Sanha Gu <64877575+9sanha@users.noreply.github.com>
2022-03-28 18:56:15 +0900

    Refactor: koa v3로 바뀜에 따라 미들웨어 수정

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import * as Koa from 'koa';
+import { connectDB } from './config/mysql'
+import * as cors from 'koa-cors'
+import * as bodyParser from 'koa-bodyparser'
 
 const app = new Koa();
+const connectDatabase = connectDB();
 const port = 3000;
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+connectDatabase.then(()=>{
+    console.log('DB')
+})
+
+app.use(async (ctx) => {
+    bodyParser()
+    cors({origin:'*'})
+    ctx.body = {msg: 'Hello World'};
 });
 
 app.listen(port);
