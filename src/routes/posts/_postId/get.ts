9969e6a0477175ef2dad3a8f85f0dfb4a714b8e0
Sanha Gu <64877575+9sanha@users.noreply.github.com>
2022-04-05 17:07:09 +0900

    Feat: 게시글 상세 조회

--- src/routes/posts/_postId/get.ts
+++ src/routes/posts/_postId/get.ts
@@ -1,10 +1,41 @@
-import { Spec } from "koa-joi-router";
+import { Joi, Spec } from "koa-joi-router";
 import { PostService } from "../../../services/post/application/service";
 import Container from "typedi";
+
+const paramsSchema = {
+    postId: Joi.number().description('게시물 아이디'),
+};
+
+const outputSchema = {
+    comments: Joi.array().required().description('댓글'),
+    text: Joi.string().required().description('본문'),
+    title: Joi.string().required().description('제목'),
+    img: Joi.string().required(),
+    id: Joi.number().required(),
+    createdAt: Joi.date().required(),
+    updatedAt: Joi.date().required(),
+};
 
 export default {
     path: '/posts/:postId',
     method: 'get',
+    meta: {
+        swagger: {
+            summary: '게시물 상세 조회',
+            descriptions: '게시글의 전체 본문과 댓글을 볼 수 있다.',
+            tags: ['posts']
+        }
+    },
+    validate: {
+        params: paramsSchema,
+        output: {
+            200:{
+                body: {
+                    data: {...outputSchema},
+                }
+            },
+        },
+    },
     handler: async (ctx) => {
         
         const postId  = Number(ctx.request.params.postId);
@@ -13,6 +44,6 @@ export default {
         
         const data = await service.getPostById(postId); 
         
-        ctx.body = data;
-    } 
+        ctx.body = { data };
+    }
 }as Spec;
--- src/routes/posts/index.ts
+++ src/routes/posts/index.ts
@@ -4,5 +4,4 @@ import { updateRoutes } from './update';
 import { deleteRoutes } from './delete';
 import { postIdRoutes } from './_postId';
 
-
 export const postsRoutes = [...deleteRoutes, ...updateRoutes, ...postIdRoutes, post, get];
