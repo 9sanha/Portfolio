c53b2ea87962c3fa19d160acff9a09e0e5dab0d0
Sanha Gu <64877575+9sanha@users.noreply.github.com>
2022-04-06 13:57:01 +0900

    Refactor: boom 수정

--- src/services/post/application/service.ts
+++ src/services/post/application/service.ts
@@ -1,50 +1,42 @@
-import {Inject, Service} from 'typedi'
-import {TypeOfPost, Post, TypeOfComment} from '../domain/model'
-import { PostRepository } from '../infrastructure/repository';
-import { Comment } from '../domain/model';
+import { Inject, Service } from "typedi";
+import { TypeOfPost, Post, TypeOfComment } from "../domain/model";
+import { PostRepository } from "../infrastructure/repository";
+import { Comment } from "../domain/model";
 
 @Service()
-export class PostService{
-    
+export class PostService {
     @Inject()
     private postRepository!: PostRepository;
-    
-    register(props:TypeOfPost){ 
+
+    register(props: TypeOfPost) {
         const post = Post.of(props);
         return this.postRepository.save(post);
     }
 
-    list(){
+    list() {
         return this.postRepository.find();
     }
 
-    getPostById(postId: number){
+    getPostById(postId: number) {
         // TODO: 찾을 수 없는 포스트 일 때 boom
         return this.postRepository.findById(postId);
     }
 
     delete(postId: number) {
-        this.postRepository.delete(postId);    
+        this.postRepository.delete(postId);
     }
 
-    async update(postId: number,patchData:{title?:string, text?:string}){
+    async update(postId: number, patchData: { title?: string; text?: string }) {
         const post = await this.getPostById(postId);
-        if(post){
-            post.update(patchData);
-            return this.postRepository.save(post);
-        }
+        post.update(patchData);
+        return this.postRepository.save(post);
     }
 
-    async registerComment(postId:number, props: TypeOfComment){
+    async registerComment(postId: number, props: TypeOfComment) {
         const post = await this.getPostById(postId);
-        
         const comment = Comment.of(props);
-        
-        if (post){     
-            post.addComment(comment);
-            const comments =  (await this.postRepository.save(post)).comments;
-            return comments[comments.length-1];
-        }
-        
+        post.addComment(comment);
+        const comments = (await this.postRepository.save(post)).comments;
+        return comments[comments.length - 1];
     }
 }
--- src/services/post/infrastructure/repository.ts
+++ src/services/post/infrastructure/repository.ts
@@ -1,30 +1,30 @@
-import {Service} from 'typedi'
-import { AppDataSource } from '../../../config/typeorm';
-import { Post } from '../domain/model';
-import { badRequest } from '../../../const/boom';
+import { Service } from "typedi";
+import { AppDataSource } from "../../../config/typeorm";
+import { Post } from "../domain/model";
+import { badRequest } from "../../../const/boom";
 @Service()
-export class PostRepository{
-    
+export class PostRepository {
     private repository = AppDataSource.getRepository(Post);
-    
-    save(post: Post){        
-        return this.repository.save(post);
-    };
 
-    async find(){
+    save(post: Post) {
+        return this.repository.save(post);
+    }
+
+    async find() {
         const posts = await this.repository.find();
         return posts;
-    };
+    }
 
-    async findById(id: number){
-        const post1 =  await this.repository.findOneOrFail({relations: ['comments'], where: {id}}).catch(()=>{
-            throw badRequest.NO_EXIST_ENTITY_ERROR;
-        });
-        
-        return post1;
-    };
+    async findById(id: number) {
+        const post = await this.repository
+            .findOneOrFail({ relations: ["comments"], where: { id } })
+            .catch(() => {
+                throw badRequest.NO_EXIST_ENTITY_ERROR;
+            });
+        return post;
+    }
 
     delete(postId: number) {
         this.repository.softDelete(postId);
-    };
+    }
 }
